bf07d2146b8fed3f5451d667cbf5273ce27322ba
Gulaya <example@gmail.com>
2022-07-28 21:42:33 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,139 @@
+// "use strict";
+
+// a=3;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth=1;
+
+// number = 10;
+// console.log(number);
+
+// // leftBorderWidth=10;
+// // console.log(number)
+
+// const obj={
+//     a:50
+// };
+
+// obj.a=10;
+// console.log(obj);
+
+
+// var name='ivan';
+// console.log(name);
+
+
+// {
+//     var result='oooo';
+// }
+
+// console.log(result);
+
+
+// let number = 4.3;
+
+// console.log(-4/0);
+// console.log('bike'*6 );
+
+// const person= 'alex';
+
+// // console.log(something);
+// let und;
+// console.log(und);
+
+// const obj={
+//     name:'alex',
+//     age:98,
+//     isMarried:true
+
+// };
+
+// console.log(obj.age);
+// console.log(obj['name']);
+
+// let arr=['plum.png','dom','apple',8]
+// console.log(arr[0])
+
+// 12
+// const arr=[1,2,3];
+// arr[22]=5959;
+
+
+// console.log(arr);
+
+
+
+
+// const obj={
+//     anna:800,
+//     alice:500
+// };
+
+// obj.nana=67;
+
+// console.log(obj['nana']);
+// console.log(obj.nana)
+// console.log(obj.alice)
+
+// const b='b';
+// obj[b]=[];
+// console.log(obj.b);
+
+// 14
+
+// alert('hello');
+
+// const result=confirm('are u here?');
+// console.log(result);
+
+// const answer=+prompt('are u 18?','18');
+// console.log(answer + 5);
+
+// const answer=+prompt('are u 18?','18');
+// console.log(typeof(answer));
+
+const answers=[];
+
+// answers[0]=prompt('what is ur name?','');
+// answers[1]=prompt('what is ur surname?','');
+// answers[2]=prompt('what is ur age?','');
+
+// console.log(typeof(answers));
+
+// console.log(typeof(null));
+
+// 14
+
+// const category='toys';
+// // console.log('https://someurl.com/'+ category + '/' + 4);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user='ivan';
+// alert(`hi,${user}`);
+
+// 15
+
+console.log('arr'+'-object');
+console.log(4 + +'-popo');
+
+let incr=10,
+    decr=10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked=true,
+      isClosed=true,
+      isOpen=false;
+
+console.log(isChecked && isClosed && isOpen)
+
+console.log(3 !== '9')
+
+
+
+
